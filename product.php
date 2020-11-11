a8e0efa105a7105fbdae0e93a7be23fad89609da
manuelhimself <manuel.pe97a.aema@gmail.com>
2020-11-11 08:27:07 +0100

    added db

--- product.php
+++ product.php
@@ -4,24 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products</title>
-    <link rel="stylesheet" href="css/U1Card.css">
+    <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
 <body>
-
-    
-    <?php }
-    } else {
-        echo "0 results";
-    }
-    $conn->close();
-    ?>
-
-    <!--Cards-->
-    <div id="cardsContainer" class="container">
-        <div class="row justify-content-center">
+    <div class="item-container">
             <?php
             include "dbCredentials.php";
 
@@ -31,8 +19,8 @@
                 if ($conn->connect_error) {
                     die("Connection failed: " . $conn->connect_error);
                     }
-
-                    $sql = "SELECT id, name, price, description FROM product";
+                    $id=$_GET['id'];
+                    $sql = "SELECT id, name, price, description FROM product WHERE id=$id";
                     $result = $conn->query($sql);
 
                     if ($result->num_rows > 0) {
@@ -41,21 +29,29 @@
                         echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
 
             ?>
-            <div class="col mb-4">
-                <div class="card h-100 shadow" style=" background-color: #222222;">
-                    <div class="inner">
-                        <img class="card-img-top" src="img/<?php $row["id"] ?>.jpg" alt="3DFX Voodoo 2 12MB">
-                    </div>
-                    <div class="card-body text-center">
-                        <h5 class="card-title"><?php $row["name"] ?></h5>
-                        <p class="card-text"><?php $row["description"] ?></p>
-                        <p class="card-text font-weight-bold">Price: $ <?php $row["price"] ?></p>
-                        <div class="card-footer">
-                            <a href="product.php?id=<?php $row["id"] ?>" class="btn" style="background-color:#880101; color: white;">Go to product</a>
-                        </div>
-                    </div>
+            <div class="container">
+            <div class="col-md-12">
+                <div class="product col-md-3 service-image-left">
+
+                    <center>
+                        <img id="item-display" src="img/<?php $row["id"] ?>.jpg" alt=""></img>
+                    </center>
                 </div>
             </div>
+
+            <div class="col-md-7">
+                <div class="product-title"><?php $row["name"] ?></div>
+                <div class="product-desc"><?php $row["description"] ?></div>
+                <div class="product-rating"><i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star-o"></i> </div>
+                <hr>
+                <div class="product-price">$ <?php $row["price"] ?></div>
+                <div class="product-stock">In Stock</div>
+                <hr>
+                <div class="btn-group cart">
+                    <a href="chart.php?id=<?php $row["id"] ?>" class="btn" style="background-color:#880101; color: white;">Add to chart</a>
+                </div>
+            </div>
+        </div>
             <?php
                         }
                         } else {
@@ -69,5 +65,3 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js " integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q " crossorigin="anonymous "></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js " integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl " crossorigin="anonymous "></script>
 </body>
-
-</html>

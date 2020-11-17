33cc891cc69884f028d65ee6c76ad76395c6bcff
manuelhimself <manuel.pe97a.aema@gmail.com>
2020-11-17 13:08:37 +0100

    Update add.php

--- add.php
+++ add.php
@@ -10,17 +10,15 @@ if ($conn->connect_error) {
 $name = $_REQUEST["name"];
 $price = $_REQUEST["price"];
 $description = $_REQUEST["description"];
+$sql = "INSERT INTO product (name, price, description) 
+        VALUES ($name, $price, $description)";
 
-$sql = "INSERT INTO product (name, price, description) VALUES ($name, $price, $description);";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-    }
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
 } else {
-    echo "0 results";
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
+mysqli_close($conn);
 
-$conn->close();
-
+?>

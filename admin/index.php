c5bffb2ac621db442d1d1bf3bc6a09ac5dc432f0
manuelhimself <manuel.pe97a.aema@gmail.com>
2020-11-26 18:29:16 +0100

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -2,13 +2,13 @@
     require_once "language.php";
 ?>
 <!DOCTYPE html>
-<html lang="<?php echo $lang ?>">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shop.me</title>
-    <link rel="stylesheet" href="../css/productStyle.css">
+    <link rel="stylesheet" href="css/productStyle.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 

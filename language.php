3370751def13229ae1349666295df3d62273cae1
manuelhimself <manuel.pe97a.aema@gmail.com>
2020-11-26 17:31:31 +0100

    added language

---
+++ language.php
@@ -0,0 +1,6 @@
+<?php
+$language = array(
+    "en" => array("Products", "Add Products", "Price", "In stock", "Add to cart", "Shopping Cart", "Name", "Price in USD", "Description", "Image"),
+    "cat" => array("Productes", "Afegeix Products", "Preu", "En estoc", "Afegeix al carretó", "Carretó", "Nom", "Preu en dòlrs americans", "Descripció", "Imatge")
+);
+?>

cddd78175f43a75dc981554ba5f8a440baf70e9d
manuelhimself <manuel.pe97a.aema@gmail.com>
2020-12-18 00:15:29 +0100

    Update add.php

--- admin/add.php
+++ admin/add.php
@@ -8,14 +8,21 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO product (name, price, description) 
-        VALUES ('$_REQUEST[name]','$_REQUEST[price]','$_REQUEST[description]')";
+$sql = "INSERT INTO product (name, price) 
+        VALUES ('$_REQUEST[name]','$_REQUEST[price]')";
 
 if (mysqli_query($conn, $sql)) {
   echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
+
+/*$sql="SELECT AUTO_INCREMENT
+FROM  INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'shop'
+AND   TABLE_NAME   = 'product';";*/
+
+
 
 mysqli_close($conn);
 

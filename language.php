5ddfb3bf426089f9eb7f01d6ea33141a6ceea4bf
manuelhimself <manuel.pe97a.aema@gmail.com>
2020-12-18 00:24:23 +0100

    Update language.php

--- language.php
+++ language.php
@@ -1,5 +1,5 @@
 <?php
-$lang = "en";
+$lang = "ca";
 $languages = array(
     "en" => array("Products", "Add Products", "Price", "In stock", "Add to cart", "Shopping Cart", "Name", "Price in USD", "Description", "Image"),
     "ca" => array("Productes", "Afegeix Productes", "Preu", "En estoc", "Afegeix al carretó", "Carretó", "Nom", "Preu en dòlrs americans", "Descripció", "Imatge")
